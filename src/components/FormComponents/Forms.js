2ffaf6992bf61851591b5a0758910db28ca4b966
Rakesh Kumar Jena <rjena025@hotmail.com>
2021-07-30 22:07:37 +0530

    fourth commit

--- src/components/FormComponents/Forms.js
+++ src/components/FormComponents/Forms.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
-
 function Forms(props) {
     const [titles, setTitle] = useState('')
     const [prices, setPrices] = useState('')
     const [dates, setDate] = useState('')
+    const [showResults, setShowResults] = useState(false)
+
+    let onClick = () => {
+        setShowResults(true)
+        let btn = document.getElementsByClassName('btn')
+        for (var i = 0; i < btn.length; i += 1) {
+            btn[i].style.display = 'none';
+        }
+    }
 
     let title = (e) => {
         setTitle(e.target.value)
@@ -32,25 +40,31 @@ function Forms(props) {
         setTitle('')
         setPrices('')
         setDate('')
+
     }
+
     return (
-        <Form onSubmit={formSubmit} autoComplete='off'>
-            <Form.Group className="mb-3 text-center" controlId="formBasicTitle">
-                <Form.Label className='text-light'>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter title" value={titles} onChange={title} />
-            </Form.Group>
-            <Form.Group className="mb-3 text-center" controlId="formBasicAmount">
-                <Form.Label className='text-light'>Amount</Form.Label>
-                <Form.Control type="text" placeholder="Enter amount" value={prices} onChange={price} />
-            </Form.Group>
-            <Form.Group className="mb-3 text-center" controlId="formBasicCheckbox">
-                <Form.Label className='text-light'>Date</Form.Label>
-                <Form.Control type="date" value={dates} onChange={date} />
-            </Form.Group>
-            <Button variant="primary text-dark" type="submit">
-                Add Expense
-            </Button>
-        </Form>
+        <>
+            <Button className='btn btn-success  text-dark' onClick={onClick} >Click Here To Add Expense &#8594;</Button>
+            {showResults ?
+                <Form onSubmit={formSubmit} autoComplete='off'>
+                    <Form.Group className="mb-3 text-center" controlId="formBasicTitle">
+                        <Form.Label className='text-light'>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Enter title" value={titles} onChange={title} />
+                    </Form.Group>
+                    <Form.Group className="mb-3 text-center" controlId="formBasicAmount">
+                        <Form.Label className='text-light'>Amount</Form.Label>
+                        <Form.Control type="text" placeholder="Enter amount" value={prices} onChange={price} />
+                    </Form.Group>
+                    <Form.Group className="mb-3 text-center" controlId="formBasicCheckbox">
+                        <Form.Label className='text-light'>Date</Form.Label>
+                        <Form.Control type="date" value={dates} onChange={date} />
+                    </Form.Group>
+                    <Button variant="primary text-dark" type="submit">
+                        Add Expense
+                    </Button>
+                </Form> : null}
+        </>
     )
 }
 
